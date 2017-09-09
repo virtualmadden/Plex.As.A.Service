5bf7377e48d1f9a42bebf3e9d90b686ae90ae94a
Jon Madden <jmadden@incomm.com>
2017-09-08 17:38:28 -0700

    Changing to .net core 2

--- UnitTests/Plex.As.A.Service.Tests/Class1.cs
+++ UnitTests/Plex.As.A.Service.Tests/Class1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Plex.As.A.Service.Tests
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
