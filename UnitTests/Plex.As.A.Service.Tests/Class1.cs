d71d46cc909fe6d49ae5cbb259cbd4e74869bcb6
Jon Madden <jmadden@incomm.com>
2017-09-08 17:43:47 -0700

    Packages

--- UnitTests/Plex.As.A.Service.Tests/Class1.cs
+++ UnitTests/Plex.As.A.Service.Tests/Class1.cs
@@ -2,11 +2,8 @@
 
 namespace Plex.As.A.Service.Tests
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        
     }
 }
